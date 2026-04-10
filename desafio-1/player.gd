c0981126f9d03c7b47dee000ab7009a2cc403dd7
orianamartina <orianapettinelli04@gmail.com>
2026-04-10 00:11:51 -0300

    desafio_1

---
+++ desafio-1/player.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+@export var speed = 400 # How fast the player will move (pixels/sec).
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("move_left"):
+		velocity.x -= 1
+	
+	if velocity.length() > 0:
+		velocity = velocity * speed
+		
+	position += velocity * delta
