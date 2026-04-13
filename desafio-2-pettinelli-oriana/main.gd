4846ae9f6765b7244e2cbb6ede4982444cc43499
orianamartina <orianapettinelli04@gmail.com>
2026-04-12 21:41:34 -0300

    fix d 2

--- desafio-2-pettinelli-oriana/main.gd
+++ desafio-2-pettinelli-oriana/main.gd
@@ -1,7 +1,5 @@
 extends Node
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$Player.set_projectile_container(self)
 	$Turret.set_values($Player, self)
