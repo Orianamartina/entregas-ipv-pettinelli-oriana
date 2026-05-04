c1efac9186bacae8440be0f7594e600227a9a238
orianamartina <orianapettinelli04@gmail.com>
2026-05-04 00:21:28 -0300

    desafio 4 listo, desafio 5 wip

---
+++ desafio-5-pettinelli-oriana/src/entities/player/JumpState.gd
@@ -0,0 +1,46 @@
+extends PlayerState
+var jumps: int = 0
+@export var jumps_limit: int = 1
+
+func enter() -> void:
+	character.velocity.y -= character.jump_speed
+	character._play_animation("jump")
+
+
+func exit() -> void:
+	jumps = 0
+
+
+func handle_input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump") && jumps < jumps_limit:
+		jumps += 1
+		character.velocity.y -= character.jump_speed
+		character._play_animation("jump")
+
+
+# En esta función vamos a manejar las acciones apropiadas para este estado
+func update(delta: float) -> void:
+	character._handle_move_input(delta)
+	if character.h_movement_direction == 0:
+		character._handle_deacceleration(delta)
+	character._apply_movement(delta)
+	if character.is_on_floor():
+		if character.h_movement_direction == 0:
+			emit_signal("finished", "idle")
+		else:
+			emit_signal("finished", "walk")
+	else:
+		if character.velocity.y > 0:
+			character._play_animation("fall")
+		else:
+			character._play_animation("jump")
+
+func _on_animation_finished(anim_name: StringName) -> void:
+	pass
+	
+func handle_event(event: StringName, value = null) -> void:
+	match event:
+		&"hit":
+			character._handle_hit(value)
+			if character.dead:
+				finished.emit(&"dead")
